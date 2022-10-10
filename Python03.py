ba8fec7e897b9c62de59794e478b4d4cd857d99c
frederikkruusmae <115454921+frederikkruusmae@users.noreply.github.com>
2022-10-10 11:47:52 +0300

    Add files via upload

---
+++ Python03.py
@@ -0,0 +1,32 @@
+#F.Kruusimäe
+#10.10.22
+#Kilpkonn
+import turtle
+
+konn1 = turtle.Turtle()
+
+
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Kilpkonna harjutus 03")
+
+
+konn1.forward(100)
+konn1.left(90)
+konn1.forward(25)
+konn1.left(90)
+konn1.forward(100)
+konn1.left(90)
+konn1.forward(25)
+konn1.left(90)
+konn1.forward(100)
+konn1.right(90)
+konn1.forward(50)
+konn1.left(90)
+konn1.forward(25)
+konn1.left(90)
+konn1.forward(75)
+konn1.left(90)
+konn1.forward(60)
+konn1.right(90)
+konn1.forward(65)
