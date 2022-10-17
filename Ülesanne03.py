112ad20a414685472934e7fed472ed31899b6b53
frederikkruusmae <115454921+frederikkruusmae@users.noreply.github.com>
2022-10-17 15:17:10 +0300

    Add files via upload

--- Ülesanne03.py
+++ Ülesanne03.py
@@ -1,9 +1,49 @@
-#F.Kruusimae
-#11.10.2022
-#Ülesanne03
-import math
+#F.Kruusimäe
+#11.10.22
+#harjutus04
 
-#korralik nimi
-nimi = input("Lisa nimi: ")
-puh_nimi = (nimi.strip()).capitalize()
-print("Tere",puh_nimi+"!")
+#palindroom
+# Drive code
+
+num = 1221
+temp = num
+reverse = 0
+while temp > 0:
+    remainder = temp % 10
+    reverse = (reverse * 10) + remainder
+    temp = temp // 10
+if num == reverse:
+  print('Palindrome')
+else:
+  print("Not Palindrome")
+
+
+
+
+
+
+#tundide arv
+a1,a2 = "8:30","14:15"
+h1,m1 = a1.split(":")
+minut1 = int(h1)*60+int(m1)
+h2,m2 = a2.split(":")
+minut2 = int(h2)*60+int(m2)
+ajavahe = minut2-minut1
+hh = ajavahe//60
+mm = ajavahe%60
+print(f"Ajavahe on {hh}:{mm}")
+
+
+
+#email
+email = "sasdas@asdasd.rer"
+print("@" in email)
+
+
+
+
+
+#vandumine
+tekst = input("ütle kurat küll!:")
+print(tekst.lower().replace("kurat","***"))
+
---
+++ Ülesanne04.py
@@ -0,0 +1,33 @@
+#F.Kruusimäe
+#17.10.2022
+#harjutus04
+
+#Juubel
+sp = "5.6.2002"
+d,m,y = sp.split(".")
+vanus = 2022-int(y)
+
+if vanus%5 == 0:
+    print("JUUBEL")
+else: ("EI OLE JUUBEL")
+
+
+
+#matem
+a,b = 10,20
+tehe = input("Vali tehe (+ - * /): ")
+if tehe=="+":
+    vastus = a + b
+elif tehe=="-":
+    vastus = a - b
+
+else:
+    vastus = "n/a"
+print(f"{a} {tehe} {b} = {vastus} ")
+
+#ruut
+a,b = 10,20
+if a==b:
+    print(f"{a} ja {b} teevad kokku ruudu")
+else:
+    print(f"{a} ja {b} teevad kokku ristküliku")
