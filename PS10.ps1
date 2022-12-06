116d0652da36f7702b87591f009dc8de46cafdd1
frederikkruusmae <115454921+frederikkruusmae@users.noreply.github.com>
2022-12-06 11:39:35 +0200

    Add files via upload

---
+++ PS10.ps1
@@ -0,0 +1,13 @@
+﻿cls
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+
+$xml = [xml](Get-Content $dir\customers.xml)
+
+$rows = $xml.customers.customer
+
+ForEach($row in $rows){
+    if( $row.country -eq "Bolivia"){
+        $row.full_name}}
