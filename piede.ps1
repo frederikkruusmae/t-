165a5db0ee1b0b31349cb6717fab29046cbe6cfb
frederikkruusmae <115454921+frederikkruusmae@users.noreply.github.com>
2023-02-02 11:29:00 +0200

    Add files via upload

---
+++ piede.ps1
@@ -0,0 +1,33 @@
+﻿#Frederik Kruusimäe
+#01.02.2023
+#Powershell KT
+
+
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+$fail = Import-csv $dir/MOCK_DATA.csv | Select-Object ip_address -skip 1
+new-item $dir\ip.txt
+
+foreach($ip in $fail){
+    $terveip = $ip.ip_address
+    $osa = $terveip.Split(".")
+    $uusip = ""
+    foreach($iposa in $osa)
+    {
+        $iposa = [int]$iposa
+        if ($iposa -ge 0 -and $iposa -le 255){
+            $uusip += [string]$iposa+"."
+        }  
+        elseif ($iposa -lt 0 -or $iposa -gt 255)
+        {
+            $uusip = ""
+            break
+        }
+    }
+    if ($uusip -ne ""){
+        $uusip = $uusip.TrimEnd(".")
+        $uusip >> $dir\ip.txt
+    }
+}
